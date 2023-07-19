7e700c433ca51e73cfae40f6474b76b9f1a0baab
Rodrigo <rodrigorael53@gmail.com>
2023-07-19 12:01:53 -0300

    :recycle: refactor: navigation button component

--- web/src/app/panel/components/Navigation.tsx
+++ web/src/app/panel/components/Navigation.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
+import { NavigationButton } from './NavigationButton'
 
 export function Navigation() {
   return (
     <nav className='mt-20 flex items-center justify-center gap-14 mb-20'>
-      <Link href={''} className='w-48 h-44 py-14 px-8 bg-[#D9D9D9] text-black text-xl text-center'>Gerir <br/> Informações</Link>
-      <Link href={''} className='w-48 h-44 pt-12 pb-14 px-8 bg-[#D9D9D9] text-black text-xl text-center'>Adicionar<br/>Aulas <br/>Código</Link>
-      <Link href={''} className='w-48 h-44 pt-12 pb-14 px-8 bg-[#D9D9D9] text-black text-xl text-center'>Adicionar <br/> Calendário <br/> Código</Link>
-      <Link href={''} className='w-48 h-44 py-14 px-8 bg-[#D9D9D9] text-black text-xl text-center'>Gerir <br/> Alunos</Link>
-      <Link href={''} className='w-48 h-44 pt-16 pb-14 px-8 bg-[#D9D9D9] text-black text-xl text-center'>Sair</Link>
+      <NavigationButton href={''} title='Gerir Informações' />
+      <NavigationButton href={''} title='Adicionar Aulas Código' />
+      <NavigationButton href={''} title='Adicionar Calendário Código' />
+      <NavigationButton href={''} title='Gerir Alunos' />
+      <NavigationButton href={''} title='Sair' />
     </nav>
   )
 }
