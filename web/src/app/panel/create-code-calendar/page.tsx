f9c907892f24d8171b06b17c5031ceab1612502c
Jovi-souza <jv479439@gmail.com>
2023-08-01 03:37:27 -0300

    create-code-calendar

--- web/src/app/panel/create-code-calendar/page.tsx
+++ web/src/app/panel/create-code-calendar/page.tsx
@@ -1,5 +1,23 @@
+import { Button } from "@/components/Button";
+import { SelectComponent } from "@/components/Select";
+
+import PdfIcon from '../../../assets/PDF-Icon.svg'
+import Image from "next/image";
+
 export default function CreateCodeCalendar() {
   return (
-    <h1>Adicionar calendário código</h1>
+    <form className="flex flex-col gap-[2.08rem] mr-auto mt-24 mb-16">
+      <fieldset className='flex items-center gap-14'>
+        <label htmlFor="information-name" className='text-xs'>Nome da Categoria</label>
+        <SelectComponent placeHolder='Mes X' data={[]}/>
+      </fieldset>
+      <div className="flex justify-between items-end">
+        <div className="flex bg-[#F3F3F3] border border-dashed border-red-500 gap-8 items-center px-3 py-3 w-56 rounded">
+          <Image src={PdfIcon} alt='PDF File' />
+          <p className="text-black text-xs font-bold underline">File Browse</p>
+        </div>
+        <Button title="Publicar" type='submit' className="h-max"/>
+      </div>
+    </form>
   )
 }
