06d6691f2e0077ac5bb4018b3887b8ea8516f02f
Jovi-souza <jv479439@gmail.com>
2023-08-02 00:38:56 -0300

    create code calendar

--- web/src/app/panel/create-code-calendar/page.tsx
+++ web/src/app/panel/create-code-calendar/page.tsx
@@ -1,10 +1,26 @@
+'use client'
+
+import { ChangeEvent, useState } from "react";
 import { Button } from "@/components/Button";
 import { SelectComponent } from "@/components/Select";
 
-import PdfIcon from '../../../assets/PDF-Icon.svg'
 import Image from "next/image";
+import PdfIcon from '../../../assets/PDF-Icon.svg'
 
 export default function CreateCodeCalendar() {
+  const [file, setFile] = useState<File | undefined>(undefined)
+
+  function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files!![0]
+    const maxSize = 10 * 1024 * 1024 // 10MB
+    
+    if (file && file.size <= maxSize) {
+      setFile(event.target.files!![0] as File)
+    } else {
+      return window.alert('Imagem muito grande!, Por favor selecione uma imagem de até 10mb')
+    }
+  }
+
   return (
     <form className="flex flex-col gap-[2.08rem] mr-auto mt-24 mb-16">
       <fieldset className='flex items-center gap-14'>
@@ -12,10 +28,13 @@ export default function CreateCodeCalendar() {
         <SelectComponent placeHolder='Mes X' data={[]}/>
       </fieldset>
       <div className="flex justify-between items-end">
-        <div className="flex bg-[#F3F3F3] border border-dashed border-red-500 gap-8 items-center px-3 py-3 w-56 rounded">
-          <Image src={PdfIcon} alt='PDF File' />
-          <p className="text-black text-xs font-bold underline">File Browse</p>
-        </div>
+        <fieldset className="bg-[#F3F3F3] border border-dashed border-red-500 px-3 py-3 w-56 rounded">
+          <label htmlFor="fileInput" className="flex gap-8 items-center cursor-pointer">
+            <Image src={PdfIcon} alt='PDF File' />
+            <p className="text-black text-xs font-bold underline">{file ? file?.name : 'File Browse' }</p>
+          </label>
+          <input id='fileInput' accept=".pdf" name='fileInput' onChange={handleFileChange} type="file" className="hidden"/>
+        </fieldset>
         <Button title="Publicar" type='submit' className="h-max"/>
       </div>
     </form>
