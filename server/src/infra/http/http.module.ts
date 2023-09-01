094e0fb5ce8ee341081071cda1b85ae8c713f9fd
Rodrigo <rodrigorael53@gmail.com>
2023-09-01 11:56:34 -0300

    :sparkles: feat: add providers and controllers

--- server/src/infra/http/http.module.ts
+++ server/src/infra/http/http.module.ts
@@ -11,6 +11,9 @@ import { SchoolController } from './controllers/school.controller'
 import { TestController } from './controllers/test.controller'
 import { PaymentController } from './controllers/payment.controller'
 import { InstallmentsController } from './controllers/installments.controller'
+import { ClassController } from './controllers/class.controller'
+import { ScheduledClassController } from './controllers/scheduled-class.controller'
+import { UserController } from './controllers/user.controller'
 
 // INFORMATION
 import { CreateInformation } from '../../application/use-cases/information/create-information'
@@ -53,6 +56,30 @@ import { CreateInstallments } from 'src/application/use-cases/installments/creat
 import { GetInstallmentsById } from 'src/application/use-cases/installments/get-installments-by-id'
 import { UpdateInstallments } from 'src/application/use-cases/installments/update-installments'
 
+// CLASS
+import { CreateClass } from 'src/application/use-cases/class/create-class'
+import { GetClassById } from 'src/application/use-cases/class/get-class-by-id'
+import { GetManyClasses } from 'src/application/use-cases/class/get-many-classes'
+import { GetManyClassesByCategory } from 'src/application/use-cases/class/get-many-classes-by-category'
+
+// SCHEDULE CLASS
+import { CreateScheduledClass } from 'src/application/use-cases/scheduled-class/create-scheduled-class'
+import { GetManyScheduledClasses } from 'src/application/use-cases/scheduled-class/get-many-scheduled-classes'
+import { GetManyScheduledClassesByClass } from 'src/application/use-cases/scheduled-class/get-many-scheduled-classes-by-class'
+import { GetManyScheduledClassesByStudent } from 'src/application/use-cases/scheduled-class/get-many-scheduled-classes-by-student'
+import { GetScheduledClassById } from 'src/application/use-cases/scheduled-class/get-scheduled-class-by-id'
+import { UpdateScheduledClass } from 'src/application/use-cases/scheduled-class/update-scheduled-class'
+import { UpdateScheduledClassStatus } from 'src/application/use-cases/scheduled-class/update-scheduled-class-status'
+
+// USER
+import { CreateUser } from 'src/application/use-cases/user/create-user'
+import { CreateUserSession } from 'src/application/use-cases/user/create-user-session'
+import { GetManyUsers } from 'src/application/use-cases/user/get-many-users'
+import { GetManyUsersBySchool } from 'src/application/use-cases/user/get-many-users-by-school'
+import { GetUserByEmail } from 'src/application/use-cases/user/get-user-by-email'
+import { GetUserById } from 'src/application/use-cases/user/get-user-by-id'
+import { UpdateUser } from 'src/application/use-cases/user/update-user'
+
 import { GenerateHash } from 'src/helpers/generate-hash'
 import { CompareHash } from 'src/helpers/compare-hash'
 import { CreateToken } from 'src/helpers/create-token'
@@ -67,6 +94,9 @@ import { CreateToken } from 'src/helpers/create-token'
     TestController,
     PaymentController,
     InstallmentsController,
+    ClassController,
+    ScheduledClassController,
+    UserController,
   ],
   providers: [
     // INFORMATION
@@ -109,6 +139,30 @@ import { CreateToken } from 'src/helpers/create-token'
     CreateInstallments,
     GetInstallmentsById,
     UpdateInstallments,
+
+    // CLASS
+    CreateClass,
+    GetClassById,
+    GetManyClasses,
+    GetManyClassesByCategory,
+
+    // SCHEDULE CLASS
+    CreateScheduledClass,
+    GetManyScheduledClasses,
+    GetManyScheduledClassesByClass,
+    GetManyScheduledClassesByStudent,
+    GetScheduledClassById,
+    UpdateScheduledClass,
+    UpdateScheduledClassStatus,
+
+    // USER
+    CreateUser,
+    CreateUserSession,
+    GetManyUsers,
+    GetManyUsersBySchool,
+    GetUserByEmail,
+    GetUserById,
+    UpdateUser,
 
     GenerateHash,
     CompareHash,
