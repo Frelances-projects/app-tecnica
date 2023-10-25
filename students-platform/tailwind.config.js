beef4dfc3257a99e38cbe9fc706fc9bc08a87e64
Jovi-souza <jv479439@gmail.com>
2023-10-25 12:00:43 -0300

    fix tailwind

--- students-platform/tailwind.config.js
+++ students-platform/tailwind.config.js
@@ -16,7 +16,25 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        light: 'Inter_300Light',
+        regular: 'Inter_400Regular',
+        medium: 'Inter_500Medium',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
       colors: {
+        primary: {
+          500: '#E3000F',
+        },
+        dark: {
+          500: '#33363F',
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
